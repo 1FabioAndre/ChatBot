88e78152d6eda8d2c5e124e4654392f60655ff21
Fabio Andre <fabio.mojica@ucb.edu.bo>
2024-02-15 11:00:31 -0400

    Saludando por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,17 @@
+// import saludar from "./saludar";
 
 const nombre_input = document.querySelector("#nombre");
+const form = document.querySelector("#saludar-form");
+const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
 
-  div.innerHTML = "<p> Hola" + nombre + "</p>";
+  div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
 });
+
+function saludar(nombre){
+  return nombre;
+}
